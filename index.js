5c5a6d2cb2645d988de288270fad68e4fab16726
Jim <jim@advancebass.com>
2022-12-05 14:08:42 -0800

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5050;
+
+app.use(express.json());
+app.use(cors());
+
+const warehouseRoutes = require('./routes/warehouseRoute');
+const inventoryRoutes = require('./routes/inventoryRoute');
+
+app.get('/', (_req, res) => {
+  res.send("Welcome to my API");
+});
+
+app.use('/warehouses', warehouseRoutes);
+app.use('/inventories', inventoryRoutes);
+
+app.listen(PORT, () => {
+  console.log(`running at http://localhost:${PORT}`);
+});
